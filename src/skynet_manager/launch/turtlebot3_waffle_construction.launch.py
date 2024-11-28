6485a90bd0763fb475123f9e04b0c660b3353404
Anirudh Swarankar <swarankar.ani24@gmail.com>
2024-11-28 10:46:11 -0500

    TASK 12.1 added launch file for turtlebot3 in construction world

---
+++ src/skynet_manager/launch/turtlebot3_waffle_construction.launch.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+def generate_launch_description():
+    headless = DeclareLaunchArgument('gui', default_value='true',
+                          description='Set to "false" to run headless.')
+    launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    x_pose = LaunchConfiguration('x_pose', default='0.0')
+    y_pose = LaunchConfiguration('y_pose', default='0.0')
+
+    world = os.path.join(
+        get_package_share_directory('skynet_manager'),
+        'worlds',
+        'construction_site_modv1.world'
+    )
+
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world}.items()
+    )
+
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+        )
+    )
+
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')
+        ),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+
+    spawn_turtlebot_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
+        ),
+        launch_arguments={
+            'x_pose': x_pose,
+            'y_pose': y_pose
+        }.items()
+    )
+
+    # ld = LaunchDescription()
+    # ld.add_action(headless)
+    # # Add the commands to the launch description
+    # ld.add_action(gzserver_cmd)
+    # ld.add_action(gzclient_cmd)
+    # ld.add_action(robot_state_publisher_cmd)
+    # ld.add_action(spawn_turtlebot_cmd)
+
+    return LaunchDescription([
+        headless,
+        gzserver_cmd,
+        robot_state_publisher_cmd,
+        spawn_turtlebot_cmd,
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+            ),
+            condition=IfCondition(LaunchConfiguration('gui'))
+        )
+    ])
