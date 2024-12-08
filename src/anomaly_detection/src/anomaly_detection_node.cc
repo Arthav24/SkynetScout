03f1e3d90a4bfa7148d42295754b06cd0dd865a8
Anirudh Swarankar <swarankar.ani24@gmail.com>
2024-12-08 13:59:58 -0500

    TASK 14.4 Anomaly Detection : Executable for anomaly detection

---
+++ src/anomaly_detection/src/anomaly_detection_node.cc
@@ -0,0 +1,32 @@
+// Copyright 2024 SkynetScout
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+* @file anomaly_detection_node.cc
+ * @brief This is executable for module anomaly detection
+*/
+
+#include "AnomalyDetection.h"
+#include <rclcpp/rclcpp.hpp>
+
+int main(int argc, char * argv[])
+{
+
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<scout::AnomalyDetection>();
+  RCLCPP_INFO(node->get_logger(),"Initiating module: Anomaly Detection");
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
