b9ebdb560cafa838fe1f4835580ed01116cd60a7
Anirudh Swarankar <swarankar.ani24@gmail.com>
2024-12-11 15:55:01 -0500

    TASK 18.3
    Documentation for Anomaly Detection

--- src/anomaly_detection/include/AnomalyBase.h
+++ src/anomaly_detection/include/AnomalyBase.h
@@ -12,27 +12,49 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//
-// Created by arthavnuc on 12/3/24.
-//
+/**
+ * @file AnomalyBase.h
+ * @brief This header file includes declaration for Anomaly detection base class
+ *        which is the base for crack detection, misaligned beams and hazardous 
+ *        object detection.
+ * @version 1.5
+ * @date 2024-12-03
+ * @author Anirudh S, Amogha Sunil
+ * @copyright Copyright (c) 2024 
+ */
 
 #ifndef BUILD_SRC_ANOMALY_DETECTION_INCLUDE_ANOMALYBASE_H_
 #define BUILD_SRC_ANOMALY_DETECTION_INCLUDE_ANOMALYBASE_H_
 
+// Include necessary OpenCV headers for image processing
 #include <opencv4/opencv2/opencv.hpp>
+// Include Skynet message types for AnomalyStatus
 #include <skynet_interfaces/msg/anomaly_status.hpp>
 
 namespace scout {
-// Using factory pattern for Anomaly concrete classes
+
+// Using the factory pattern for anomaly concrete classes
 /**
- * @class Anomaly
- * @brief This class is base class for all possible anomaly detection classes.
+ * @class AnomalyBase
+ * @brief This class serves as the base class for all anomaly detection classes. 
+ *        It defines a common interface for different types of anomaly detection 
+ *        (such as crack detection, beam misalignment, and hazardous object detection).
  */
 class AnomalyBase {
 public:
+  /**
+   * @brief Virtual destructor for cleaning up derived class instances
+   */
   virtual ~AnomalyBase() {}
+
+  /**
+   * @brief Pure virtual function to process an image and detect anomalies
+   * @param image The image to process (in OpenCV format)
+   * @return AnomalyStatus message with the detection results
+   */
   virtual skynet_interfaces::msg::AnomalyStatus processImage(cv::Mat) = 0;
 };
+
 } // namespace scout
 
 #endif // BUILD_SRC_ANOMALY_DETECTION_INCLUDE_ANOMALYBASE_H_
--- src/anomaly_detection/src/anomaly_detection_node.cc
+++ src/anomaly_detection/src/anomaly_detection_node.cc
@@ -13,20 +13,35 @@
 // limitations under the License.
 
 /**
-* @file anomaly_detection_node.cc
- * @brief This is executable for module anomaly detection
-*/
+ * @file anomaly_detection_node.cc
+ * @brief This is the executable for the Anomaly Detection module. It initializes 
+ *        the ROS 2 node and starts the processing of anomaly detection tasks.
+ * @version 1.5
+ * @date 2024-12-03
+ * @author Anirudh S, Amogha Sunil
+ * @copyright Copyright (c) 2024 
+ */
 
-#include "AnomalyDetection.h"
-#include <rclcpp/rclcpp.hpp>
+#include "AnomalyDetection.h"  // Include the header for AnomalyDetection class
+#include <rclcpp/rclcpp.hpp>    // Include ROS 2 rclcpp library for node and communication
 
 int main(int argc, char * argv[])
 {
-
+  // Initialize the ROS 2 communication system
   rclcpp::init(argc, argv);
+
+  // Create an instance of the AnomalyDetection node
   auto node = std::make_shared<scout::AnomalyDetection>();
-  RCLCPP_INFO(node->get_logger(),"Initiating module: Anomaly Detection");
+
+  // Log a message indicating the initiation of the Anomaly Detection module
+  RCLCPP_INFO(node->get_logger(), "Initiating module: Anomaly Detection");
+
+  // Spin the node, keeping it active to process incoming messages and tasks
   rclcpp::spin(node);
+
+  // Shut down the ROS 2 communication system after the node has finished
   rclcpp::shutdown();
+
+  // Return 0 indicating the program has completed successfully
   return 0;
 }
